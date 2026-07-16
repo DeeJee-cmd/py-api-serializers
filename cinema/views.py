65fb73b76ff504bda6c2663240b873d1423909cb
Pasha Liashenko <zedxi236@ukr.net>
2026-07-16 22:44:43 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -48,7 +48,17 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = MovieSession.objects.select_related("movie", "cinema_hall")
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "retrieve":
+            queryset = queryset.prefetch_related(
+                "movie__genres", "movie__actors"
+            )
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
